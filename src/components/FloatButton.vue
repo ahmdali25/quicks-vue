32f8651401de3972c3c4d31d6fd26b492227730a
ahmdali25 <ahmadaliazhary@gmail.com>
2024-06-04 13:35:11 +0700

    add float action button - quicks

---
+++ src/components/FloatButton.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <v-btn
+    fab
+    width="60px"
+    height="60px"
+    color="8785FF"
+    class="ml-4"
+    @click.stop="handleShow"
+    v-bind="attrs"
+    v-on="on"
+  >
+  </v-btn>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,23 @@
 
 <template>
   <main>
-    <div>test</div>
+    <v-speed-dial location="left center" transition="slide-x-reverse-transition">
+      <template v-slot:activator="{ props: activatorProps }">
+        <v-fab v-bind="activatorProps" width="68px" height="68px" color="#2F80ED" icon>
+          <img src="@/assets/icons/quicks.svg" width="18px" alt="Quicks Logo" />
+        </v-fab>
+      </template>
+
+      <v-btn key="1" icon="$success">inbox</v-btn>
+      <v-btn key="2" icon="$success">task</v-btn>
+    </v-speed-dial>
   </main>
 </template>
+
+<style scoped>
+.v-fab {
+  position: fixed;
+  bottom: 50px;
+  right: 90px;
+}
+</style>
